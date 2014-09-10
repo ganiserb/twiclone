c8a05f00402456859661bad48157ee60b24afa10
ganiserb <g.gabriel.b@gmail.com>
2014-09-10 20:14:12 -0300

    defined profile model

--- userprofile/models.py
+++ userprofile/models.py
@@ -1,7 +1,9 @@
 from django.db import models
+from django.contrib.auth.models import User
 
-class Profile(models.Model):
+class Profile(User):
     avatar = models.ImageField(upload_to='user_avatars')
+    interest_tags = models.ManyToManyField(InterestTag, related_name='users')
 
-class InteresTag(models.Model):
+class InterestTag(models.Model):
     tag_name = models.CharField(25)
