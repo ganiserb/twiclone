18e2845703c7cbcb0052840bc1bce8c1682c5865
ganiserb <g.gabriel.b@gmail.com>
2014-09-15 18:17:10 -0300

    registered admin models

--- twicler/admin.py
+++ twicler/admin.py
@@ -1,3 +1,9 @@
+# coding=utf-8
 from django.contrib import admin
 
+from twicler.models import Twiclo, UserSettings
+
+
 # Register your models here.
+admin.site.register(Twiclo)
+admin.site.register(UserSettings)
--- userprofile/admin.py
+++ userprofile/admin.py
@@ -1,3 +1,8 @@
+# coding=utf-8
 from django.contrib import admin
 
+from userprofile.models import Profile, InterestTag
+
 # Register your models here.
+admin.site.register(Profile)
+admin.site.register(InterestTag)
