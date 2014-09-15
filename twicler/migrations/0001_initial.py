f8c73810d2d6203e9d35c9dba403d28951b055eb
ganiserb <g.gabriel.b@gmail.com>
2014-09-15 17:35:09 -0300

    fixed migrations

--- twicler/migrations/0001_initial.py
+++ twicler/migrations/0001_initial.py
@@ -16,7 +16,7 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Twiclo',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('text', models.CharField(max_length=200)),
                 ('created', models.DateTimeField(auto_now_add=True)),
             ],
@@ -27,15 +27,15 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='UserSettings',
             fields=[
-                ('user_ptr', models.OneToOneField(serialize=False, primary_key=True, parent_link=True, auto_created=True, to=settings.AUTH_USER_MODEL)),
-                ('visibility', models.CharField(max_length=2, default='PU', choices=[('PU', 'Public'), ('FI', 'Those I follow')])),
+                ('user_ptr', models.OneToOneField(parent_link=True, serialize=False, to=settings.AUTH_USER_MODEL, primary_key=True, auto_created=True)),
+                ('visibility', models.CharField(choices=[('PU', 'Public'), ('FI', 'Those I follow')], max_length=2, default='PU')),
                 ('followers', models.ManyToManyField(related_name='followers_rel_+', to='twicler.UserSettings')),
                 ('following', models.ManyToManyField(related_name='following_rel_+', to='twicler.UserSettings')),
             ],
             options={
-                'verbose_name': 'user',
-                'abstract': False,
                 'verbose_name_plural': 'users',
+                'abstract': False,
+                'verbose_name': 'user',
             },
             bases=('auth.user',),
         ),
--- userprofile/migrations/0001_initial.py
+++ userprofile/migrations/0001_initial.py
@@ -8,14 +8,14 @@ from django.conf import settings
 class Migration(migrations.Migration):
 
     dependencies = [
-        ('auth', '0001_initial'),
+        migrations.swappable_dependency(settings.AUTH_USER_MODEL),
     ]
 
     operations = [
         migrations.CreateModel(
             name='InterestTag',
             fields=[
-                ('id', models.AutoField(verbose_name='ID', serialize=False, primary_key=True, auto_created=True)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('tag_name', models.CharField(max_length=25)),
             ],
             options={
@@ -25,15 +25,13 @@ class Migration(migrations.Migration):
         migrations.CreateModel(
             name='Profile',
             fields=[
-                ('user_ptr', models.OneToOneField(serialize=False, primary_key=True, parent_link=True, auto_created=True, to=settings.AUTH_USER_MODEL)),
+                ('id', models.AutoField(serialize=False, verbose_name='ID', auto_created=True, primary_key=True)),
                 ('avatar', models.ImageField(upload_to='user_avatars')),
                 ('interest_tags', models.ManyToManyField(related_name='users', to='userprofile.InterestTag')),
+                ('user', models.OneToOneField(to=settings.AUTH_USER_MODEL)),
             ],
             options={
-                'verbose_name': 'user',
-                'abstract': False,
-                'verbose_name_plural': 'users',
             },
-            bases=('auth.user',),
+            bases=(models.Model,),
         ),
     ]
