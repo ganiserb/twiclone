ee4f734cb3c8e6782d1e991d41edb6a55ff6ecee
ganiserb <g.gabriel.b@gmail.com>
2014-09-16 19:17:00 -0300

    created basic profile page

--- userprofile/models.py
+++ userprofile/models.py
@@ -15,12 +15,12 @@ class Profile(models.Model):
     The user profile model that extends the user model with a OneToOne link
     """
 
-
     def default_profile_picture():  # TODO: Así se define una función?
         return "default.jpg"
 
     user = models.OneToOneField(User)
     avatar = models.ImageField(upload_to='user_avatars', default=default_profile_picture())     # TODO: asco Default. No funciona con lambda
+    bio = models.CharField(max_length=200)  # TODO: De qué manera obligaría a completar esto cuando se crea el usuario?
     interest_tags = models.ManyToManyField(InterestTag, related_name='users', blank=True)
 
     def __str__(self):
