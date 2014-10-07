aed58c91cc33a9e10b0ac36d95df057f78b4c64b
ganiserb <g.gabriel.b@gmail.com>
2014-10-07 14:19:49 -0300

    removed unused line

--- twiclone/settings.py
+++ twiclone/settings.py
@@ -8,10 +8,6 @@ For the full list of settings and their values, see
 https://docs.djangoproject.com/en/1.7/ref/settings/
 """
 
-# Custom impot
-from django.core.urlresolvers import reverse
-
-
 # Build paths inside the project like this: os.path.join(BASE_DIR, ...)
 import os
 BASE_DIR = os.path.dirname(os.path.dirname(__file__))
